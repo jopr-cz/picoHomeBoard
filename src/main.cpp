d0e28cd44f9062323aae1708253bd34cdce988ea
Honza silent killer <jan.opravil@jopr.cz>
2023-10-03 11:55:09 +0200

    as

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 
 #include "jolibMainHelper.h"
 
+#include "serial_linux.h"
+#include "modbus.h"
+
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
     MAIN_HELPER modul_helper;
     
+    SerialLinux ser("/dev/ttyS0");
+    modul_helper.addModul(&ser);
+    MODBUS modbus(&ser);
+    modul_helper.addModul(&modbus);
+
+
 
     time_t timestamp;
     time_t bootTime=time(NULL);
