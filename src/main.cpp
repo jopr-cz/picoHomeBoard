dd0787ef945651b4dccc13019b4e651444913cb4
Honza silent killer <jan.opravil@jopr.cz>
2023-10-03 11:23:27 +0200

    AA

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 
-#include "nanomodbus.h"
-
-
-#define COILS_ADDR_MAX 100
-
-
-nmbs_error handle_read_coils(uint16_t address, uint16_t quantity, nmbs_bitfield coils_out, uint8_t unit_id, void* arg) {
-
-    if (address + quantity > COILS_ADDR_MAX + 1)
-        return NMBS_EXCEPTION_ILLEGAL_DATA_ADDRESS;
-
-    // Read our coils values into coils_out
-    for (int i = 0; i < quantity; i++) {
-        bool value = nmbs_bitfield_read(server_coils, address + i);
-        nmbs_bitfield_write(coils_out, i, value);
-    }
-
-    return NMBS_ERROR_NONE;
-}
+#include "jolibMainHelper.h"
 
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
+    MAIN_HELPER modul_helper;
+    
+
+    time_t timestamp;
+    time_t bootTime=time(NULL);
+    uint64_t timestampUs;
+    printf("Simulator started\n");
+
+
+    while(true){
+        timestamp = time(NULL);
+        timestampUs=timestamp-bootTime;
+
+        modul_helper.loop(timestampUs);
+    }
+
+
 }
