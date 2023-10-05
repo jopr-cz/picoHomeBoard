0ada976080b2a0e3157134205734018afbac7123
Honza silent killer <jan.opravil@jopr.cz>
2023-10-05 20:52:41 +0200

    dokumentace - schema

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <sys/time.h>
 
 #include "jolibMainHelper.h"
 
 #include "serial_linux.h"
+#include "serial_linuxUDP.h"
+#include "serial_linuxTCP.h"
 #include "modbus.h"
 
 
@@ -11,22 +14,25 @@ int main()
     std::cout << "Hello World" << std::endl;
     MAIN_HELPER modul_helper;
     
-    SerialLinux ser("/dev/ttyS0");
+    //SerialLinux ser("/dev/ttyS0");
+    //SerialLinuxUDP ser;
+    SerialLinuxTCP ser;
     modul_helper.addModul(&ser);
-    MODBUS modbus(&ser);
+    MODBUS modbus(&ser,0x05);
     modul_helper.addModul(&modbus);
 
 
 
-    time_t timestamp;
-    time_t bootTime=time(NULL);
-    uint64_t timestampUs;
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    long long timestampUs;
     printf("Simulator started\n");
 
 
     while(true){
-        timestamp = time(NULL);
-        timestampUs=timestamp-bootTime;
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        timestampUs= (end_time.tv_sec - start_time.tv_sec) * 1000000LL +
+               (end_time.tv_nsec - start_time.tv_nsec) / 1000LL;
 
         modul_helper.loop(timestampUs);
     }
